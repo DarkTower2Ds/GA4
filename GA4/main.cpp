0a3f40fa85c769b363474609becc3da403957f48
DarkTower2Ds <DarkTower2Ds@users.noreply.github.com>
2017-10-22 16:13:36 -0500

    Created main.cpp with main() and empty avgOf2(), avgOf3(), and avgOf4() functions.

---
+++ GA4/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+
+}
+
+double avgOf2(Student a, Student b)
+{
+
+}
+
+double avgOf3(Student a, Student, b, Student c)
+{
+
+}
+
+double avgOf4(Student a, Student b, Student c, Student d)
+{
+
+}
